20bb25044286c2579e0de46554e387b08dcd67de
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-06-25 10:42:09 +0530

    Time: 2 ms (96.80%) | Memory: 41.7 MB (65.53%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,23 +1,28 @@
-public class Solution {
-    public boolean isValid(String s) {
+class Solution 
+{
+    public boolean isValid(String s) 
+    {
         Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-    
-            if (c == '(') {
-                stack.push(')');
-            } else if (c == '{') {
-                stack.push('}');
-            } else if (c == '[') {
-                stack.push(']');
-            } 
-                        else {
-                if (stack.isEmpty() || stack.pop() != c) {
-                    return false;
-                }
+        for(char c:s.toCharArray())
+        {
+           if (c=='(')
+           {
+            stack.push(')');
+           }
+           else if (c=='{')
+           {
+            stack.push('}');
+           }
+           else if (c=='[')
+           {
+            stack.push(']');
+           }
+           else
+            if(stack.isEmpty() || stack.pop()!=c)
+            {
+                return false;
             }
         }
-        
-        return stack.isEmpty();
+           return stack.isEmpty();
     }
 }
