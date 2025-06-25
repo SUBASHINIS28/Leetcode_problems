953d31c657d44a450c10506818ee90a4c22a1474
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-06-25 23:16:33 +0530

    Time: 2 ms (96.80%) | Memory: 41.8 MB (47.56%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,28 +1,29 @@
-class Solution 
-{
+class Solution {
     public boolean isValid(String s) 
     {
-        Stack<Character> stack = new Stack<>();
-        for(char c:s.toCharArray())
+      Stack<Character> stack = new Stack<>();
+      for(char c:s.toCharArray())
+      {
+        if (c== '(')
         {
-           if (c=='(')
-           {
             stack.push(')');
-           }
-           else if (c=='{')
-           {
+        }
+        else if (c=='{')
+        {
             stack.push('}');
-           }
-           else if (c=='[')
-           {
+        }
+        else if (c=='[')
+        {
             stack.push(']');
-           }
-           else
-            if(stack.isEmpty() || stack.pop()!=c)
+        }
+        else
+            if(stack.isEmpty() || stack.pop() !=c)
             {
                 return false;
             }
-        }
-           return stack.isEmpty();
+      }
+      return stack.isEmpty();
+
+
     }
 }
