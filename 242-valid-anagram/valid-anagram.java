23f442d46a4dec756767957eb22c12ca0ef90851
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-06-26 10:15:17 +0530

    Time: 4 ms (77.72%) | Memory: 44.5 MB (53.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,16 +1,14 @@
 class Solution {
-    public boolean isAnagram(String s, String t) 
-    {
+    public boolean isAnagram(String s, String t) {
         if(s.length() != t.length())
         {
             return false;
         }
-        char[] sArr=s.toCharArray();
-        char[] tArr=t.toCharArray();
-
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
         Arrays.sort(sArr);
         Arrays.sort(tArr);
-        
         return Arrays.equals(sArr,tArr);
+        
     }
 }
