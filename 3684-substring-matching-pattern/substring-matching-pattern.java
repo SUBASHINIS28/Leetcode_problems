822052b2c48be166a649601b84695a5fa281649e
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-06-26 10:01:41 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (94.44%) - LeetSync

--- 3684-substring-matching-pattern/substring-matching-pattern.java
+++ 3684-substring-matching-pattern/substring-matching-pattern.java
@@ -1,13 +1,15 @@
-class Solution {
+class Solution 
+{
     public boolean hasMatch(String s, String p) 
     {
         int index=p.indexOf('*');
-        int firstpos=s.indexOf(p.substring(0,index));
-        int secondpos=s.indexOf(p.substring(index+1),firstpos + index);
-        if(firstpos != -1 &&secondpos != -1)
+        int firstpos = s.indexOf(p.substring(0,index));
+        int secondpos = s.indexOf(p.substring(index+1),firstpos + index);
+        if(firstpos != -1 && secondpos != -1)
         {
             return true;
         }
         return false;
+
     }
 }
