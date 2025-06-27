8cef9810295e754aa907ec746a33db4f9f5b0094
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-06-27 11:57:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (31.88%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,18 +1,23 @@
 class Solution {
-    public double myPow(double x, int n) {
-        long N = n; // Use long to handle edge case like Integer.MIN_VALUE
-        if (N < 0) {
-            x = 1 / x;
+    public double myPow(double x, int n) 
+    {
+        long N = n;
+        if(N<0)
+        {
+            x = 1/x;
             N = -N;
         }
-        double result = 1.0;
-        while (N > 0) {
-            if ((N % 2) == 1) {
-                result *= x;
-            }
-            x *= x;
-            N /= 2;
+        double result =1.0;
+        while (N>0)
+        {
+            if((N%2==1))
+          {
+            result *=x;
+          }
+        x=x*x;
+        N=N/2;
         }
-        return result;
+    return result;
+
     }
 }
