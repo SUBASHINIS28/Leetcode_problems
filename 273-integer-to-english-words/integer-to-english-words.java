ec381bfd34b8276663fe780073345e8a15460b84
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-06-29 11:25:00 +0530

    Time: 2 ms (87.13%) | Memory: 41.6 MB (94.01%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.java
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -1,34 +1,49 @@
-public class Solution {
-    private final String[] below20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-                                      "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
-                                      "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    private final String[] thousands = {"", "Thousand", "Million", "Billion"};
-
-    public String numberToWords(int num) {
-        if (num == 0) return "Zero";
-
-        int i = 0;
+class Solution {
+    private final String[] belowTwenty={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    private final String[] tens={"","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+    public String numberToWords(int num)
+    {
+        if(num==0)
+            return "Zero";
+        return helper(num);      
+    }
+    private String helper(int num)
+    {
         StringBuilder result = new StringBuilder();
-
-        while (num > 0) {
-            int chunk = num % 1000;
-            if (chunk != 0) {
-                String chunkWords = helper(chunk).trim() + " " + thousands[i] + " ";
-                result.insert(0, chunkWords);
-            }
-            num /= 1000;
-            i++;
+        if(num<20)
+        {
+           result.append(belowTwenty[num]);
         }
-
-        return result.toString().trim();
+        else if(num<100)
+        {
+           result.append(tens[num/10]).append(" ").append(belowTwenty[num%10]);
+        }
+        else if(num<1000)
+        {
+           result.append(helper(num/100)).append(" Hundred ").append(helper(num%100));
+        }
+        else if(num<1000000)
+        {
+            result.append(helper(num/1000)).append(" Thousand ").append(helper(num%1000));
+        }
+        else if(num<1000000000)
+        {
+           result.append(helper(num/1000000)).append(" Million ").append(helper(num%1000000));        
+        }
+        else
+       {
+          result.append(helper(num/1000000000)).append(" Billion ").append(helper(num%1000000000));
+       }
+       return result.toString().trim();
     }
-
-    private String helper(int num) {
-        if (num == 0) return "";
-        else if (num < 20) return below20[num] + " ";
-        else if (num < 100) return tens[num / 10] + " " + helper(num % 10);
-        else return below20[num / 100] + " Hundred " + helper(num % 100);
-    }
-
 }
+
+
+
+
+
+
+
+
+
+   
