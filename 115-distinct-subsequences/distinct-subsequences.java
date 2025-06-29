475c856ea8b15f96b982331e96d6b41ef612d23a
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-06-29 14:23:03 +0530

    Time: 17 ms (85.63%) | Memory: 51.2 MB (65.31%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDistinct(String a, String b) 
+    {
+        int m = a.length();
+        int n = b.length();
+        int d[][] = new int[m+1][n+1];
+        for(int i=0;i<=m;i++) d[i][0] =1;
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(a.charAt(i-1) == b.charAt(j-1))
+                {
+                    d[i][j] = d[i-1][j-1] + d[i-1][j];
+                }
+               else
+               {
+                   d[i][j] = d[i-1][j];
+               }
+            }
+            
+        }
+        return d[m][n];
+    }
+       
+}
