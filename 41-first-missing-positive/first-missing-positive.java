db227dd2defad21262becd2f2b10dcb5715b5d1f
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-06-30 06:53:31 +0530

    Time: 1 ms (100.00%) | Memory: 56.9 MB (78.66%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,33 +1,33 @@
 class Solution {
     public int firstMissingPositive(int[] nums) 
     {
-       int i = 0;
-       while(i<nums.length) 
-       {
-         int curr = nums[i]-1;
-         if(curr>=0 && curr<nums.length && nums[i] !=nums[curr])
-         {
-            swap(nums,i,curr);
-         }
-         else
-         {
-            i++;
-         }
+        int i = 0;
+        while(i<nums.length)
+        {
+            int curr = nums[i]-1;
+            if(curr>=0 && curr<nums.length && nums[i] != nums[curr])
+            {
+                swap(nums,i,curr);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        for(int j=0;j<nums.length;j++)
+        {
+            if(nums[j] !=j+1)
+            {
+                return j+1;
+            }
+        }
+        return nums.length+1;
 
-       }
-       for(int j=0;j<nums.length;j++)
-       {
-           if(nums[j] != j+1)
-           {
-             return j+1;
-           }
-       }
-       return nums.length+1;
     }
     private static void swap(int[]arr,int a,int b)
     {
-        int temp = arr[a];
-        arr[a] = arr[b] ;
+        int temp= arr[a];
+        arr[a] = arr[b];
         arr[b] = temp;
     }
 }
