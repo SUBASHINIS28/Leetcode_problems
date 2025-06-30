e9dad5d85c379e79dc9cb118027ba6bb75bd6038
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-06-30 12:02:06 +0530

    Time: 2 ms (100.00%) | Memory: 45 MB (50.29%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.java
+++ 875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -2,30 +2,29 @@ class Solution {
     public int longestMountain(int[] arr) 
     {
         int n = arr.length;
-        int i = 1;
-        int maxLen = 0;
-        while(i<n-1)
+        if(n<3)
         {
-           if(arr[i-1] < arr[i] && arr[i] > arr[i+1]) 
-           {
-              int left = i-1;
-              int right = i+1;
-              while(left>0 && arr[left-1] < arr[left])
-              {
-                left--;
-              }
-              while(right<n-1 && arr[right] > arr[right+1])
-              {
-                right++;
-              }
-              maxLen=Math.max(maxLen,right-left+1);
-              i=right;
-           }
-           else
-            {
-                i++;
-            }  
+            return 0;
         }
-        return maxLen;
+        int max = 0;
+        for(int i=1;i<n-1;i++)
+        {
+            if(arr[i] > arr[i-1] && arr[i] > arr[i+1])
+            {
+                int left = i;
+                int right = i;
+                while(left>0 && arr[left-1]< arr[left])
+                {
+                    left--;
+                }
+                while(right<n-1 && arr[right+1]<arr[right])
+                {
+                    right++;
+                }
+                int curr = right-left+1;
+                max= Math.max(max,curr);
+            }
+        }
+        return max;
     }
 }
