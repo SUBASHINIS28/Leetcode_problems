6becf913fd540307845b51959bc160309a7c35a9
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-07-07 13:12:10 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (50.67%) - LeetSync

--- 330-patching-array/patching-array.java
+++ 330-patching-array/patching-array.java
@@ -1,21 +1,23 @@
 class Solution {
     public int minPatches(int[] nums, int n) 
     {
-        long miss=1;
-        int i=0,patch=0;
+        long miss = 1;
+        int i=0,patch = 0;
         while(miss<=n)
         {
-            if(i<nums.length &&  nums[i]<=miss)
+            if(i<nums.length&&nums[i]<=miss)
             {
                 miss+=nums[i];
                 i++;
             }
             else
             {
-                miss +=miss;
+                miss+=miss;
                 patch++;
             }
         }
         return patch;
+
+        
     }
 }
