e88567fcbc052b84513181dc60ec287804f93530
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-07-09 14:03:01 +0530

    Time: 31 ms (65.95%) | Memory: 61.3 MB (27.59%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,29 +1,25 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) 
     {
-       int n = nums.length;
-        int[] result = new int[n - k + 1];
-        Deque<Integer> dq = new ArrayDeque<>(); // stores indices
-
-        for (int i = 0; i < n; i++) {
-            // Remove indices of elements not in the current window
-            while (!dq.isEmpty() && dq.peekFirst() < i - k + 1) {
+        int res[] = new int[nums.length-k+1];
+        int x = 0;
+        Deque<Integer> dq = new LinkedList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!dq.isEmpty() && dq.peekFirst()<=i-k)
+            {
                 dq.pollFirst();
             }
-
-            // Remove indices of smaller elements (they won't be the max)
-            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+            while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i])
+            {
                 dq.pollLast();
             }
-
-            dq.offerLast(i); // Add current element's index
-
-            // Starting from i = k - 1, record max in the result
-            if (i >= k - 1) {
-                result[i - k + 1] = nums[dq.peekFirst()];
+            dq.offerLast(i);
+            if(i>=k-1)
+            {
+                res[x++] = nums[dq.peekFirst()];
             }
         }
-        return result;
+        return res;
     }
 }
-
