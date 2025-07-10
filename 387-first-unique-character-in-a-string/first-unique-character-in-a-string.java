870c5245b2ec416c3ac3f8c79e3c9287cd3fe96d
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-07-10 13:41:43 +0530

    Time: 26 ms (50.18%) | Memory: 45.4 MB (48.53%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -3,7 +3,7 @@ class Solution {
     {
         for(int i=0;i<s.length();i++)
         {
-            if(s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i)))
+            if(s.indexOf(s.charAt(i))== s.lastIndexOf(s.charAt(i)))
             {
                 return i;
             }
