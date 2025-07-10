3516f6b58a55e948cb9a0a288688b9d04eda2a0b
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-07-10 13:16:56 +0530

    Time: 38 ms (94.40%) | Memory: 104.8 MB (20.80%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,30 +1,29 @@
 class Solution {
     public int maxEnvelopes(int[][] envelopes) 
     {
-        Arrays.sort(envelopes,(a,b)->
+        Arrays.sort(envelopes,(a,b) ->
         {
             if(a[0]== b[0])
             {
-                return b[1] - a[1];
+                return b[1]-a[1];
             }
             return a[0] - b[0];
         });
         int dp[] = new int[envelopes.length];
-        int len = 0;
-        for(int [] envelope : envelopes)
+        int len  = 0;
+        for(int[]envelope : envelopes)
         {
             int height = envelope[1];
             int index = Arrays.binarySearch(dp,0,len,height);
             if(index<0)
             {
-               index = -(index+1);
+                index = -(index+1);
             }
             dp[index] = height;
             if(index==len)
             {
                 len++;
             }
-
         }
         return len;
     }
