16a332da45cb51182dfc7a54766bf0cac47b64a9
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-07-10 11:13:16 +0530

    Time: 40 ms (78.91%) | Memory: 104.3 MB (38.97%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,22 +1,31 @@
 class Solution {
     public int maxEnvelopes(int[][] envelopes) 
-    { Arrays.sort(envelopes, (a, b) -> {
-            if (a[0] == b[0]) return b[1] - a[1];
+    {
+        Arrays.sort(envelopes,(a,b) ->
+        {
+            if(a[0] == b[0])
+            {
+                return b[1] - a[1];
+            }
             return a[0] - b[0];
         });
+        int [] dp = new int[envelopes.length];
+        int len=0;
 
-        // Step 2: Extract heights and apply LIS
-        int[] dp = new int[envelopes.length];
-        int len = 0;
-
-        for (int[] envelope : envelopes) {
+        for(int[] envelope : envelopes)
+        {
             int height = envelope[1];
-            int idx = Arrays.binarySearch(dp, 0, len, height);
-            if (idx < 0) idx = -(idx + 1);
-            dp[idx] = height;
-            if (idx == len) len++;
+            int indx = Arrays.binarySearch(dp,0,len,height);
+            if(indx<0)
+            {
+                indx = -(indx+1);
+            }
+            dp[indx] = height;
+            if(indx==len)
+            {
+                len++;
+            }
         }
-
         return len;
     }
 }
