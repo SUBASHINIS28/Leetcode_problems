65eac3dfe60238a22616358a183c89e7879fea1e
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-07-10 13:23:24 +0530

    Time: 39 ms (87.31%) | Memory: 104.5 MB (31.66%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -5,19 +5,19 @@ class Solution {
         {
             if(a[0]== b[0])
             {
-                return b[1]-a[1];
+                return b[1] - a[1];
             }
-            return a[0] - b[0];
+            return a[0] -b[0];
         });
         int dp[] = new int[envelopes.length];
-        int len  = 0;
-        for(int[]envelope : envelopes)
+        int len  =0;
+        for(int [] envelope : envelopes)
         {
             int height = envelope[1];
             int index = Arrays.binarySearch(dp,0,len,height);
             if(index<0)
             {
-                index = -(index+1);
+               index = -(index+1);
             }
             dp[index] = height;
             if(index==len)
