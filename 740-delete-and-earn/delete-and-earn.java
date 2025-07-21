38f0987b4a271079259263730053e8f353f2b2a1
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-07-21 11:59:22 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (18.94%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,27 +1,38 @@
 class Solution {
-    public int deleteAndEarn(int[] nums)
-     {
-  
-        int max = 0;
-        for (int num : nums) 
+    public int deleteAndEarn(int[] nums) 
+    {
+        if(nums.length==1)
         {
-            max = Math.max(max, num);
-        }   
-        int[] points = new int[max + 1];
-        for (int num : nums) {
-            points[num] += num;
+            return nums[0];
         }
-        
-        int take = 0; 
-        int skip = 0; 
-        for (int i = 0; i <= max; i++) {
-            int take_i = skip + points[i]; 
-            int skip_i = Math.max(skip, take); 
-
-            take = take_i; 
-            skip = skip_i; 
+        if(nums.length==2)
+        {
+            if(Math.abs(nums[0]-nums[1])==1)
+            {
+                return Math.max(nums[0],nums[1]);
+            }
+            else
+            {
+                return nums[0]+nums[1];
+            }
         }
-
-        return Math.max(take, skip); 
+        int max=nums[0];
+        for(int num:nums){
+            if(num>max){
+                max=num;
+            }
+        }
+        int arr[] =new int[max+1];
+        for(int num:nums){
+            arr[num]+=num;
+        }
+        int prevprev=arr[0];
+        int prev=arr[1];
+        for(int i=2;i<=max;i++){
+            int curr=Math.max(arr[i]+prevprev,prev);
+            prevprev=prev;
+            prev=curr;
+        }
+        return prev;
     }
 }
