a102bf53c57654b651b5496d9144eff3e83327bd
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-07-21 11:16:58 +0530

    Time: 2 ms (99.72%) | Memory: 45.1 MB (23.63%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -3,20 +3,17 @@ class Solution {
      {
   
         int max = 0;
-        for (int num : nums) {
+        for (int num : nums) 
+        {
             max = Math.max(max, num);
-        }
-
-        
+        }   
         int[] points = new int[max + 1];
         for (int num : nums) {
             points[num] += num;
         }
-
         
         int take = 0; 
         int skip = 0; 
-
         for (int i = 0; i <= max; i++) {
             int take_i = skip + points[i]; 
             int skip_i = Math.max(skip, take); 
