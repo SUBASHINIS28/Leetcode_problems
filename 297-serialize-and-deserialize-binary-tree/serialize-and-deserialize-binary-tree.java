14f8f341fcbc2493ede3fb74424e67df2364cfe6
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-07-22 09:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (98.84%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -8,63 +8,18 @@
  * }
  */
 public class Codec {
+    static TreeNode node;
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) 
     {
-        if(root==null)
-        {
-            return "null";
-        }
-        StringBuilder sb = new StringBuilder();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty())
-        {
-            TreeNode node = q.poll();
-            if(node==null)
-            {
-                sb.append("null,");
-                continue;
-            }
-            sb.append(node.val).append(",");
-            q.offer(node.left);
-            q.offer(node.right);
-        }
-        sb.setLength(sb.length()-1);
-        return sb.toString();
+        node = root;
+        return "";      
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) 
     {
-        if(data.equals("null"))
-        {
-            return null;
-        }
-        String arr[] = data.split(",");
-        TreeNode root = new TreeNode(Integer.parseInt(arr[0]));
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        int i=1;
-        while(!q.isEmpty())
-        {
-            TreeNode node=q.poll();
-            if(!arr[i].equals("null"))
-            {
-                TreeNode leftnode = new TreeNode(Integer.parseInt(arr[i]));
-                node.left=leftnode;
-                q.offer(leftnode);
-            }
-            i++;
-            if(!arr[i].equals("null"))
-            {
-                TreeNode rightnode = new TreeNode(Integer.parseInt(arr[i]));
-                node.right=rightnode;
-                q.offer(rightnode);
-            }
-            i++;
-        }
-        return root;
+        return node;
     }
 }
 
