454e8f4e99d0fa55986ffb972d9d40fa1d88274c
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-07-27 17:55:54 +0530

    Time: 17 ms (99.90%) | Memory: 48.4 MB (94.16%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.java
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -1,15 +1,20 @@
-class RecentCounter {
-    Queue<Integer> q;
-
-    public RecentCounter() {
-        q = new LinkedList<>();
+public class RecentCounter 
+{
+    private static final int[] records = new int[10000]; 
+    private int start;
+    private int end;
+    public RecentCounter() 
+    {        
+        start = 0;
+        end = 0;
     }
-
-    public int ping(int t) {
-        q.offer(t);
-        while (q.peek() < t - 3000) {
-            q.poll();
+    public int ping(int t) 
+    {
+        while (start < end && (t - records[start] > 3000)) 
+        { 
+            start++; 
         }
-        return q.size();
+        records[end++] = t; 
+        return end - start; 
     }
 }
