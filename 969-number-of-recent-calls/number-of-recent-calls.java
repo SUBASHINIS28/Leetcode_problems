1983059369ecc6cf0b53fc6667cccbd3aa5c7f49
SUBASHINI S <153823077+SUBASHINIS28@users.noreply.github.com>
2025-07-27 18:06:01 +0530

    Time: 18 ms (96.97%) | Memory: 48.2 MB (97.08%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.java
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -3,20 +3,21 @@ class RecentCounter
     private static final int[] records = new int[10000];
     private int start;
     private int end;
+
     public RecentCounter() 
     {
-        start = 0;
+        start =0;
         end = 0;
     }
     
-    public int ping(int t)
+    public int ping(int t) 
     {
-        while(start<end && (t-records[start] > 3000))
+        while(start < end & (t-records[start] >  3000))
         {
             start++;
         }
-        records[end++]=t;
-        return  end-start;
+        records[end++] = t;
+        return end-start;
     }
 }
 
